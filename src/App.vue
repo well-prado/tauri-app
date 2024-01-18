9f65de7e09fa3ca51bf3bbab0c028b5260d1ffd8
Wellington Prado <wellington@deskree.com>
2024-01-18 14:12:41 -0300

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { WebviewWindow } from '@tauri-apps/api/window';
+
+const createWindow = async () => {
+  const newWindowMeta = { id: Date.now().toString(), title: 'Tauri Studio' };
+  const webview = new WebviewWindow(newWindowMeta.id, {
+    url: '',
+  });
+
+  await webview.emit('tauri://create-window', newWindowMeta);
+};
+</script>
+
+<template>
+  <div class="flex items-center justify-center h-screen">
+    <button @click="createWindow">Open second window</button>
+  </div>
+</template>
